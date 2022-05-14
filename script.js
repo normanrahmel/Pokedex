c20799b606558b41aca831a99a481e1b9cbd5cc2
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-05-14 11:43:46 +0200

    added new Functions

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 let pokemonJSON;
 
+
 function init() {
     renderPokedex();
 }
+
 
 async function renderPokedex() {
     let url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`;
     let response = await fetch(url);
     pokemonJSON = await response.json();
 
-    console.log('loaded Pokemon', pokemonJSON);
+    console.log('Zeig mir meine Datenstruktur', pokemonJSON);
 
     getSinglePokemonData(pokemonJSON);
-    renderPokemonInfo(pokemonJSON);
 }
 
-
-function renderPokemonInfo(pokemonJSON) {
-    document.getElementById('pokemonName').innerHTML = pokemonJSON['forms'][0]['name'];
-    document.getElementById('pokemonImg').src = pokemonJSON['sprites']['other']['dream_world']['front_default'];
-
-}
 
 async function getSinglePokemonData(pokemonJSON) {
     for (let i = 0; i < pokemonJSON.results.length; i++) {
@@ -28,6 +23,7 @@ async function getSinglePokemonData(pokemonJSON) {
         const singlePokemonURL = pokemonJSON['results'][i]['url'];
         const response = await fetch(singlePokemonURL);
         const singlePokemonJSON = await response.json();
+        renderPokemonListCard(i, singlePokemonJSON);
     }
 }
 
@@ -40,4 +36,37 @@ function renderPokemonListCard(i, singlePokemonJSON) {
     const pokemonAbility = singlePokemonJSON['abilities'][0]['ability']['name'];
     const pokemonHeight = singlePokemonJSON['height'];
     const pokemonWeight = singlePokemonJSON['weight'];
+
+    pushSinglePokemonDetails(pokemonName, pokemonImage, pokemonType, pokemonAbility, pokemonHeight, pokemonWeight);
+
+    renderPokemonListCardHTML(i, pokemonName, pokemonImage, pokemonType);
+}
+
+
+function pushSinglePokemonDetails(pokemonName, pokemonImage, pokemonType, pokemonAbility, pokemonHeight, pokemonWeight) {
+    const pokemon = {
+        'name': pokemonName,
+        'img': pokemonImage,
+        'type': pokemonType,
+        'ability': pokemonAbility,
+        'height': pokemonHeight,
+        'weight': pokemonWeight
+    }
+}
+
+
+function renderPokemonListCardHTML(i, pokemonName, pokemonImage, pokemonType) {
+
+    document.getElementById('listPokemonCards').innerHTML += /*html*/ `
+    
+        <div class="col-12 col-sm-6 col-md-4" onclick="openDetailView(${i})">
+            <div class="card mb-4 list__card shadow-sm hvr-float-shadow">
+                <img id="listPokemonImage" class="list__pokemon__image"  src="${pokemonImage}" alt="${pokemonName}">
+                <div class="card-body">
+                    <h5 id="listPokemonName" class="card-title">${pokemonName}</h5>
+                    <span id="listPokemonFeature" class="card__text card__text__bg__${pokemonType}">${pokemonType}</span>
+                </div>
+            </div>
+        </div>
+        `;
 }
